1978d27811f22a1cd1ed1f0965004aa82f7a8b9e
AlexBryksin <alexander.bryksin@yandex.ru>
2019-12-25 13:48:45 +0300

    add initial server setup middlewares and mlab mongodb

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-console.log('JS =D')
+const dotenv = require('dotenv')
+const express = require('express')
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+const hpp = require('hpp')
+const cors = require('cors')
+const morgan = require('morgan')
+const colors = require('colors')
+
+const connectDB = require('./db/db')
+
+// Load .env files
+dotenv.config({ path: './config/config.env' })
+
+// Connect DB
+connectDB()
+
+// Init Express
+const app = express()
+
+// Body parser
+app.use(express.json())
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+// Sanitize data
+// https://blog.websecurify.com/2014/08/hacking-nodejs-and-mongodb.html
+app.use(mongoSanitize())
+
+// Set security headers
+app.use(helmet())
+
+// Prevent XSS attacks
+app.use(xss())
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100,
+})
+app.use(limiter)
+
+// Prevent http param pollution
+app.use(hpp())
+
+// Enable CORS
+app.use(cors())
+
+const PORT = process.env.PORT || 5000
+// Run Server
+const server = app.listen(PORT, () => console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red)
+  // Close server & exit process
+  server.close(() => process.exit(1))
+})
