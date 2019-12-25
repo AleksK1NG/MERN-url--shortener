bf0a3d4ee3bba1df7568b22298e41318ee1f6a28
AlexBryksin <alexander.bryksin@yandex.ru>
2019-12-25 14:27:54 +0300

    add logout handler

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router()
 const authController = require('../controllers/authController')
 const auth = require('../middlewares/authMiddleware')
 
+router.get('/me', auth.authMiddleware, authController.getCurrentUser)
 router.post('/register', authController.registerUser)
 router.post('/login', authController.loginUser)
-router.get('/me', auth.authMiddleware, authController.getCurrentUser)
+router.get('/logout', authController.logout)
 
 module.exports = router
