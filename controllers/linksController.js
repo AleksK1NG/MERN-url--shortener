ec02e40397ac968caf45030ee35ab7e8aaf4c06f
Alexander <alexander.bryksin@yandex.ru>
2019-12-26 10:36:21 +0300

    add links initial controller

--- controllers/linksController.js
+++ controllers/linksController.js
@@ -2,26 +2,22 @@ const User = require('../models/User')
 const Link = require('../models/Link')
 const ErrorResponse = require('../utils/errorsResponse')
 const asyncMiddleware = require('../middlewares/asyncMiddleware')
-
+const shortid = require('shortid')
 
 // @POST Get all bootCamps | Public
 // api/v1/links
 exports.generateLink = asyncMiddleware(async (req, res) => {
-  // res.results comes from reqResMiddleware
-  res.status(200).json(res.results)
+  res.status(201).json({})
 })
 
 // @GET Get all bootCamps | Public
 // api/v1/links
 exports.getAllLinks = asyncMiddleware(async (req, res) => {
-  // res.results comes from reqResMiddleware
-  res.status(200).json(res.results)
+  res.status(200).json({})
 })
-
 
 // @GET Get all bootCamps | Public
 // api/v1/links/:id
 exports.getLinkById = asyncMiddleware(async (req, res) => {
-  // res.results comes from reqResMiddleware
-  res.status(200).json(res.results)
+  res.status(200).json({})
 })
