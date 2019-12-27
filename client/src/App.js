7e7f5871fe0f664812a1e1d5007f1e799bc19668
Alexander <alexander.bryksin@yandex.ru>
2019-12-27 15:27:06 +0300

    add ProtectedRoute and materialize css

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
 import React, { Suspense } from 'react'
 import { connect } from 'react-redux'
-import './App.css'
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary'
 import { Route, Switch } from 'react-router-dom'
+import 'materialize-css'
 
 const Routes = React.lazy(() => import('./routes/routes'))
 
 const App = () => {
   return (
     <>
-      <Switch>
-        <ErrorBoundary>
-          <Suspense fallback={<p>Loading ...</p>}>
-            <Route component={Routes} />
-          </Suspense>
-        </ErrorBoundary>
-      </Switch>
+      <div className="container">
+        <Switch>
+          <ErrorBoundary>
+            <Suspense fallback={<p>Loading ...</p>}>
+              <Route component={Routes} />
+            </Suspense>
+          </ErrorBoundary>
+        </Switch>
+      </div>
     </>
   )
 }
--- client/src/store/modules/authModule/authEpics.js
+++ client/src/store/modules/authModule/authEpics.js
@@ -5,21 +5,38 @@ import { ajax } from 'rxjs/ajax'
 import { registerUserError, registerUserSuccess } from './authActions'
 import { of } from 'rxjs'
 
-const TEST_URL = 'https://jsonplaceholder.typicode.com/users/1'
+const TEST_URL = 'https://jsonplaceholder.typicode.com/users/'
 
-const jsonFetchEpic = (action$, state) => {
+// const jsonFetchEpic = (action$, state) => {
+//   return action$.pipe(
+//     ofType(authTypes.REGISTER_USER_REQUEST),
+//     mergeMap((action) => {
+//       debugger
+//       return ajax.getJSON(`${TEST_URL}/${action.payload.userData}`).pipe(
+//         tap((data) => console.log(data)),
+//         map((response) => {
+//           return registerUserSuccess(response)
+//         }),
+//         catchError((err) => of(registerUserError(err))),
+//       )
+//     }),
+//   )
+// }
+
+const registerUserEpic = (action$, state) => {
   return action$.pipe(
     ofType(authTypes.REGISTER_USER_REQUEST),
-    mergeMap((action) =>
-      ajax.getJSON(TEST_URL).pipe(
+    mergeMap((action) => {
+      debugger
+      return ajax.getJSON(`${TEST_URL}/${action.payload.userData}`).pipe(
         tap((data) => console.log(data)),
         map((response) => {
           return registerUserSuccess(response)
         }),
         catchError((err) => of(registerUserError(err))),
-      ),
-    ),
+      )
+    }),
   )
 }
 
-export default [jsonFetchEpic]
+export default [registerUserEpic]
