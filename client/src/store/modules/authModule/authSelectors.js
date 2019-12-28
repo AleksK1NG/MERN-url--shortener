2bb9e956c4ac83a830f7ccba8dc92bf400b32591
Alexander <alexander.bryksin@yandex.ru>
2019-12-28 13:06:31 +0300

    add disable auth buttons on loading

--- client/src/store/modules/authModule/authSelectors.js
+++ client/src/store/modules/authModule/authSelectors.js
@@ -7,3 +7,4 @@ export const authStateSelector = (state) => state.auth
 export const nameSelector = createSelector(authStateSelector, (state) => state.name)
 export const userSelector = createSelector(authStateSelector, (state) => (state.user ? state.user : null))
 export const isAuthenticatedSelector = createSelector(authStateSelector, (state) => state.isAuthenticated)
+export const isAuthLoadingSelector = createSelector(authStateSelector, (state) => state.isLoading)
