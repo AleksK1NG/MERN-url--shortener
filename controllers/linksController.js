6144260877e870cd762927a66865705086be60b8
Alexander <alexander.bryksin@yandex.ru>
2019-12-28 13:52:10 +0300

    add server links controller

--- controllers/linksController.js
+++ controllers/linksController.js
@@ -6,18 +6,42 @@ const shortid = require('shortid')
 
 // @POST Get all bootCamps | Public
 // api/v1/links
-exports.generateLink = asyncMiddleware(async (req, res) => {
-  res.status(201).json({})
+exports.generateLink = asyncMiddleware(async (req, res, next) => {
+  const baseUrl = process.env.BASE_URL
+  const { from } = req.body
+
+  const code = shortid.generate()
+
+  const isExists = await Link.findOne({ from })
+  if (isExists) return next(new ErrorResponse('Link already exists', 400))
+
+  const to = baseUrl + '/t/' + code
+
+  const link = await Link.create({
+    code,
+    to,
+    from,
+    owner: req.user._id,
+  })
+
+  res.status(201).json({ data: link })
 })
 
 // @GET Get all bootCamps | Public
 // api/v1/links
 exports.getAllLinks = asyncMiddleware(async (req, res) => {
-  res.status(200).json({})
+  const { user } = req
+
+  const links = await Link.find({ owner: user._id })
+
+  res.status(200).json({ data: links })
 })
 
 // @GET Get all bootCamps | Public
 // api/v1/links/:id
-exports.getLinkById = asyncMiddleware(async (req, res) => {
-  res.status(200).json({})
+exports.getLinkById = asyncMiddleware(async (req, res, next) => {
+  const link = await Link.findById(req.params.id)
+  if (!link) return next(new ErrorResponse('Link not found', 400))
+
+  res.status(200).json({ data: link })
 })
